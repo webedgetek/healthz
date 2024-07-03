f0f41c0a6bbc707d3db8f00c5722117e16088bc2
Mohammed Alhassan <bigboss200535@gmail.com>
2024-07-03 19:59:04 +0000

    Update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,27 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('firstname');
-             $table->string('othername');
+            $table->string('firstname', 150);
+            $table->string('othername', 150);
+            $table->string('name')->virtualAs("CONCAT(firstname, ' ', othername)");
+            $table->string('username', 150);
+            $table->string('password', 100);
+            $table->string('salt', 100)->nullable();
+            $table->string('telephone', 50);
+            $table->string('gender', 100);
+            $table->string('mode', 100);
+            $table->string('mode', 100);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('email_verified_at', 50)->nullable();
+            $table->string('added_id', 100);
+            $table->string('added_date', 100);
+            $table->string('udpated_by', 100);
+            $table->string('status', 100);
+            $table->string('archived', 100);
+            $table->string('archived_by', 100);
+            $table->date('archived_date', 100);
+            // $table->rememberToken();
+            // $table->timestamps();
         });
     }
 
