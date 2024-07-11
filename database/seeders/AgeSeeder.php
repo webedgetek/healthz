3fadf958412f348d6653f90de510dbefcef96167
Mohammed Alhassan <alhassan.mohammedga@gmail.com>
2024-07-11 18:29:05 +0000

    Age seeder correction

--- database/seeders/AgeSeeder.php
+++ database/seeders/AgeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Age;
+use App\Models\User;
 
 class AgeSeeder extends Seeder
 {
