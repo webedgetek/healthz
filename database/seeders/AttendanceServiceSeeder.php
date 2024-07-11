5f8abc6c71c2457a0821787edbd5742a1277c5d8
Mohammed Alhassan <alhassan.mohammedga@gmail.com>
2024-07-11 19:21:53 +0000

    Attendance Service Seeder update

--- database/seeders/AttendanceServiceSeeder.php
+++ database/seeders/AttendanceServiceSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\AttendanceService;
 use App\Models\User;
+use App\Models\Gender;
 
 class AttendanceServiceSeeder extends Seeder
 {
@@ -17,10 +18,234 @@ class AttendanceServiceSeeder extends Seeder
     public function run()
     {
         $user = User::inRandomOrder()->first(); 
+        $gender = Gender::inRandomOrder()->first(); 
+        $age = Age::inRandomOrder()->first(); 
 
-        $gender = AttendanceService::create([
-            'gender_id' => '1',
-            'gender' => 'All',
+        $service = AttendanceService::create([
+            'att_service_id' => '0001',
+            'description' => 'General Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0002',
+            'description' => 'Direct Service',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0003',
+            'description' => 'Antenatal',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0004',
+            'description' => 'Postnatal',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0005',
+            'description' => 'In-Patient',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0006',
+            'description' => 'Physiotherapy',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0007',
+            'description' => 'Eye Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0008',
+            'description' => 'Paediatrics',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0009',
+            'description' => 'Gynaecology',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0010',
+            'description' => 'Ent Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0011',
+            'description' => 'Dental Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0012',
+            'description' => 'Medical Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0013',
+            'description' => 'Paediatric Surgery',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0014',
+            'description' => 'Adult Surgery',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0015',
+            'description' => 'Labour Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0016',
+            'description' => 'Othorpaedics Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0017',
+            'description' => 'Urology Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0018',
+            'description' => 'Surgical Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0019',
+            'description' => 'Dietetic Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0020',
+            'description' => 'Specialist Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
+            'user_id' => $user->user_id,
+            'added_date' => now(),
+            'status' => 'Active',
+            'archived' => 'No',
+        ]);
+
+        $service = AttendanceService::create([
+            'att_service_id' => '0020',
+            'description' => 'Diabetic Consultation',
+            'gender_id' => $gender->gender_id,
+            'age_id' => $age->age_id,
             'user_id' => $user->user_id,
             'added_date' => now(),
             'status' => 'Active',
